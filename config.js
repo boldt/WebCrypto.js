c2d3c2808b81a4d1cb54386414c09fa626e107c9
Dennis Boldt <boldt@itm.uni-luebeck.de>
2017-01-28 16:33:33 +0100

    Clean up.

--- config.js
+++ config.js
@@ -17,7 +17,6 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.7",
-    "shims": "github:boldt/adapters-browser@master",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
